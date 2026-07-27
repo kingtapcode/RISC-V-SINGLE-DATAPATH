13d2b6d1ccec00411f47e858043b706cd0fc6e69
Minh Trí <maiquangminhtri2007@gmail.com>
2026-07-27 21:20:18 +0700

    Refactor PCsel module for improved jump logic
    Refactor PCsel module to prioritize JALR and simplify selection logic.

--- RTL/PCsel.sv
+++ RTL/PCsel.sv
@@ -6,17 +6,14 @@ module PCsel(
     output logic [1:0] pc_sel_o
 );
 always_comb begin
-    pc_sel_o = 2'b00; // mặc định là PC + 4
-    case ({jal_i, jalr_i, branch_i, zero_i})
-    4'b0000: pc_sel_o = 2'b00; // PC + 4
-    4'b0010: pc_sel_o = 2'b00; // PC + 4
-    4'b0011: pc_sel_o = 2'b01; // PC + offset
-    4'b1000: pc_sel_o = 2'b01; // PC + offset
-    4'b0100: pc_sel_o = 2'b10; // PC + ALU
-    default: pc_sel_o = 2'b00;
-    endcase
+    if (jalr_i) begin
+        pc_sel_o = 2'b10; // Ưu tiên JALR: Nhảy tới RS1 + imm
+    end 
+    else if (jal_i || (branch_i && zero_i)) begin
+        pc_sel_o = 2'b01; // JAL hoặc BEQ Taken: Nhảy tới PC + imm
+    end 
+    else begin
+        pc_sel_o = 2'b00; // Mặc định đi tiếp PC + 4
+    end
 end
 endmodule
-
-
-
